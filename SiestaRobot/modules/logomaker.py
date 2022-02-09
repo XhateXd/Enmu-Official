760981c6591f714b4ae7a314dc169739c6c18224
Kokushibo-Kizuki-1 <98401272+Kokushibo-kizuki-1@users.noreply.github.com>
2022-02-09 15:40:31 +0530

    Update logomaker.py

--- SiestaRobot/modules/logomaker.py
+++ SiestaRobot/modules/logomaker.py
@@ -197,7 +197,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
+            caption="Logo by [Enmu](https://t.me/Enmu_kizuki_bot)",
             force_document=False,
         )
         os.remove(flnme)
