de59ef0bd39776ee74c15d4089e047375483d8f4
Kokushibo-Kizuki-1 <98401272+Kokushibo-kizuki-1@users.noreply.github.com>
2022-02-13 21:31:11 +0530

    Update karma.py

--- SiestaRobot/modules/karma.py
+++ SiestaRobot/modules/karma.py
@@ -37,7 +37,7 @@ regex_downvote = r"^(\-|\-\-|\-1|👎|noob|weak)$"
 )
 @capture_err
 async def upvote(_, message):
-    if not await is_karma_on(message.chat.id):
+    if not is_karma_on(message.chat.id):
         return
     if not message.reply_to_message.from_user:
         return
@@ -48,18 +48,14 @@ async def upvote(_, message):
     chat_id = message.chat.id
     user_id = message.reply_to_message.from_user.id
     user_mention = message.reply_to_message.from_user.mention
-    current_karma = await get_karma(
-        chat_id, await int_to_alpha(user_id)
-    )
+    current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
     if current_karma:
-        current_karma = current_karma['karma']
+        current_karma = current_karma["karma"]
         karma = current_karma + 1
     else:
         karma = 1
     new_karma = {"karma": karma}
-    await update_karma(
-        chat_id, await int_to_alpha(user_id), new_karma
-    )
+    await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     await message.reply_text(
         f"Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}"
     )
